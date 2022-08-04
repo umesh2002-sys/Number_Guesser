f63fd4b8b69267122b1ea14b0f4134c3658f9a5e
umesh2002-sys <umesh.shrestha2002@gmail.com>
2022-08-04 12:03:49 +0545

    second commit

--- number_guesser.py
+++ number_guesser.py
@@ -10,14 +10,11 @@ def mode_easy():
     print('Picking Number ...')
     time.sleep(3)
     user_guess = int(input('What is your guess?: '))
+    lives -= 1
     correct_answer = random.randint(1, 10)
 
     while user_guess != correct_answer:
-        lives -= 1
-        if user_guess == correct_answer:
-            print(f'You have guess the correct number {correct_answer}')
-            user_exit()
-        elif user_guess > correct_answer:
+        if user_guess > correct_answer:
             print(f'You have guess the higher. Now, your lives remain {lives}')
             user_guess = int(input('What is your guess?: '))
         elif user_guess < correct_answer:
@@ -27,7 +24,11 @@ def mode_easy():
         if lives == 1:
             print(f'You lose. The correct guess was {correct_answer}')
             user_exit()
-
+        lives -= 1
+        
+    if user_guess == correct_answer:
+            print(f'You have guess the correct number {correct_answer}')
+            user_exit()
 
 def mode_normal():
     lives = 5
@@ -36,14 +37,11 @@ def mode_normal():
     print('Picking Number ...')
     time.sleep(3)
     user_guess = int(input('What is your guess?: '))
+    lives -= 1
     correct_answer = random.randint(1, 50)
 
     while user_guess != correct_answer:
-        lives -= 1
-        if user_guess == correct_answer:
-            print(f'You have guess the correct number {correct_answer}')
-            user_exit()
-        elif user_guess > correct_answer:
+        if user_guess > correct_answer:
             print(f'You have guess the higher. Now, your lives remain {lives}')
             user_guess = int(input('What is your guess?: '))
         elif user_guess < correct_answer:
@@ -52,6 +50,11 @@ def mode_normal():
 
         if lives == 1:
             print(f'You lose. The correct guess was {correct_answer}')
+            user_exit()
+        lives -= 1
+        
+    if user_guess == correct_answer:
+            print(f'You have guess the correct number {correct_answer}')
             user_exit()
 
 def mode_hard():
@@ -61,14 +64,11 @@ def mode_hard():
     print('Picking Number ...')
     time.sleep(3)
     user_guess = int(input('What is your guess?: '))
+    lives -= 1
     correct_answer = random.randint(1, 100)
 
     while user_guess != correct_answer:
-        lives -= 1
-        if user_guess == correct_answer:
-            print(f'You have guess the correct number {correct_answer}')
-            user_exit()
-        elif user_guess > correct_answer:
+        if user_guess > correct_answer:
             print(f'You have guess the higher. Now, your lives remain {lives}')
             user_guess = int(input('What is your guess?: '))
         elif user_guess < correct_answer:
@@ -78,6 +78,47 @@ def mode_hard():
         if lives == 1:
             print(f'You lose. The correct guess was {correct_answer}')
             user_exit()
+        lives -= 1
+        
+    if user_guess == correct_answer:
+            print(f'You have guess the correct number {correct_answer}')
+            user_exit()
+
+
+def mode_cosm():
+    print('Costom mode')
+    try:
+        lives = int(input('Enter how much lives do you want to access for guess: '))
+        min = int(input('min number: '))
+        max = int(input('maxnumber: '))
+    except:
+        print('Invalid input. Enter valid number.')
+    
+    print(f'Yoo mate you have choose the hard mode. Guess number between {min} to {max}')
+    time.sleep(3)
+    print('Picking Number ...')
+    time.sleep(3)
+    user_guess = int(input('What is your guess?: '))
+    lives -= 1
+    correct_answer = random.randint(min, max)
+
+    while user_guess != correct_answer:
+        if user_guess > correct_answer:
+            print(f'You have guess the higher. Now, your lives remain {lives}')
+            user_guess = int(input('What is your guess?: '))
+        elif user_guess < correct_answer:
+            print(f'You have guess the lower. Now, your lives remain {lives}')
+            user_guess = int(input('What is your guess?: '))
+
+        if lives == 1:
+            print(f'You lose. The correct guess was {correct_answer}')
+            user_exit()
+        lives -= 1
+        
+    if user_guess == correct_answer:
+            print(f'You have guess the correct number {correct_answer}')
+            user_exit()
+    
 
 def user_exit():
     user_exit = input('Do you want to exit? (yes/no):').lower()
@@ -90,15 +131,17 @@ def user_exit():
 
 
 def guess_game():
-    print('----------------------------------------------')
-    print('|                                            |')
-    print('|Hello! fellow mate Welcome to the guess game|')
-    print('|                                            |')
-    print('|        Enter easy for easy mode            |')
-    print('|        Enter normal for normal mode        |')
-    print('|        Enter hard for the hard mode        |')
-    print('|                                            |')
-    print('----------------------------------------------')
+    print('--------------------------------------------------')
+    print('|                                                |')
+    print('|  Hello! fellow mate Welcome to the guess game  |')
+    print('|                                                |')
+    print('|          Enter easy for easy mode              |')
+    print('|          Enter normal for normal mode          |')
+    print('|          Enter hard for the hard mode          |')
+    print('|          Enter Cosm for costom game            |')
+    print('|                                                |')
+    print('|                                                |')
+    print('------------------------------------------------')
     game_mode = input('Enter the game mode: ').lower()
 
     if game_mode == 'easy':
@@ -107,6 +150,8 @@ def guess_game():
         mode_normal()
     elif game_mode == 'hard':
         mode_hard()
+    elif game_mode == 'cosm':
+        mode_cosm()
     else:
         print('Fellow met you have enter the wrong input')
         guess_game()
